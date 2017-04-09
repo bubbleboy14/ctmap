fcb26b3bb5de6aa5ce599b28a53aa5ff701094ac
bubbleboy14 <mario.balibrera@gmail.com>
2017-04-09 03:58:21 -0700

    init: scrapers dir; _scrape.py sym; /_scrape route

--- ctmap/init.py
+++ ctmap/init.py
@@ -1,17 +1,21 @@
 import os
 
 idir = os.path.join("img", "map")
-dirs = [idir, os.path.join("js", "kinds")]
+dirs = [idir, os.path.join("js", "kinds"), "scrapers"]
 copies = {
 	"css": ["custom.css"]
 }
 copies[idir] = ["business.png", "commercial.png", "house.png",
 	"industrial.png", "office.png", "one-family.png", "residential.png"]
 syms = {
+	".": ["_scrape.py"],
 	"css": ["map.css"],
 	"html": ["map"],
 	"js": ["map"]
 }
 model = {
 	"ctmap.model": ["*"]
+}
+routes = {
+	"/_scrape": "_scrape.py"
 }
