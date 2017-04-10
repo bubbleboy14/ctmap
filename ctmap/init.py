50e3f39b191d58df70431cad59ffc9f3b7113483
bubbleboy14 <mario.balibrera@gmail.com>
2017-04-09 18:17:51 -0700

    init: added livee img dirs/copies

--- ctmap/init.py
+++ ctmap/init.py
@@ -1,12 +1,21 @@
 import os
 
 idir = os.path.join("img", "map")
-dirs = [idir, os.path.join("js", "kinds"), "scrapers", os.path.join("scrapers", "data")]
+ldir = os.path.join(idir, "live")
+objd = os.path.join(ldir, "objective")
+obsd = os.path.join(ldir, "obstacle")
+scod = os.path.join(ldir, "scout")
+dirs = [idir, ldir, objd, obsd, scod, os.path.join("js", "kinds"),
+	"scrapers", os.path.join("scrapers", "data")]
 copies = {
 	"css": ["custom.css"]
 }
 copies[idir] = ["business.png", "commercial.png", "house.png",
 	"industrial.png", "office.png", "one-family.png", "residential.png"]
+copies[ldir] = ["clip.png", "peep.png", "remote.png", "zap.png"]
+copies[objd] = ["default.png", "red.png", "green_arrow.png", "red_arrow.png"]
+copies[obsd] = ["default.png"]
+copies[scod] = ["default.png", "panic.png"]
 syms = {
 	".": ["_scrape.py"],
 	"css": ["map.css"],
