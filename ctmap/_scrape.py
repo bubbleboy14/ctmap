ea368ae3fe6749d9407608aec4d1d881d146f401
bubbleboy14 <mario.balibrera@gmail.com>
2017-04-09 18:35:46 -0700

    /_scrape handler: fail() if MAP_SCRAPERS is not configured

--- ctmap/_scrape.py
+++ ctmap/_scrape.py
@@ -1,8 +1,10 @@
 from importlib import import_module
-from cantools.web import respond, cgi_get
+from cantools.web import respond, cgi_get, fail
 from cantools import config
 
 def response():
+	if not config.map or not config.map.scrapers:
+		fail("no scrapers configured! (define MAP_SCRAPERS, '|'-delimited, in your config)")
 	scrape_target = cgi_get("scraper", choices=config.map.scrapers.split("|"))
 	import_module("scrapers.%s"%(scrape_target,)).full_scan()
 
