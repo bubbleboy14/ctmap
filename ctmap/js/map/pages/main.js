235f5551082b7ad412d38e11f45e471b03011d53
bubbleboy14 <mario.balibrera@gmail.com>
2019-11-08 16:40:27 -0800

    main page loader: support cfg.header flag

--- ctmap/js/map/pages/main.js
+++ ctmap/js/map/pages/main.js
@@ -1,14 +1,22 @@
 CT.require("CT.all");
 CT.require("core");
-CT.setVal("mapkey", CT.data.choice(core.config.ctmap.geokeys));
+var cfg = core.config.ctmap;
+CT.setVal("mapkey", CT.data.choice(cfg.geokeys));
 CT.require("CT.map", true);
 CT.require("map.core");
-CT.map.util.setGeoKeys(core.config.ctmap.geokeys);
-CT.net.setCache(core.config.ctmap.netcache);
+CT.map.util.setGeoKeys(cfg.geokeys);
+CT.net.setCache(cfg.netcache);
 CT.net.setSpinner(true);
 CT.map.useSingleInfoWindow();
 
 CT.onload(function() {
+	var ctm = CT.dom.id("ctmain");
+	if (cfg.header)
+		CT.initCore();
+	else
+		ctm.style.top = CT.dom.id("ctheader").style.height = "0px";
+	ctm.style.padding = "10px"; // aligns with map margins
+
 	CT.log.startTimer("load");
 	map.core.controller.init();
 	CT.dom.id("clearcache").onclick = CT.storage.clear;
